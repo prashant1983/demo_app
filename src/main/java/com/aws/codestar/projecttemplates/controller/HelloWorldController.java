2c910f003f3276fde0e8b7187147e95ffb51b041
EC2 Default User <ec2-user@ip-172-31-13-70.ec2.internal>
2020-12-29 03:09:21 +0000

    compilatoins issues

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Basic Spring web service controller that handles all GET requests.
